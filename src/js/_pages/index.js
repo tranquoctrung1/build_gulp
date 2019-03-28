77382eeb6547aa5532dde67a2c6ab84098a49dfd
tranquoctrung <tranquoctrung.10k13@gmail.com>
2019-03-28 20:43:33 +0700

    add new

--- src/js/_pages/index.js
+++ src/js/_pages/index.js
@@ -11,26 +11,54 @@ $("#res").on('click',function()
 	$(this).attr('href','./about.html')
 })
 
+$("#show1").on('click',(e) =>{
+	var x = e.target.src.slice(21);
+	$("#show").attr('src',`.${x}`)
+})
+$("#show2").on('click',(e) =>{
+	var x = e.target.src.slice(21);
+	$("#show").attr('src',`.${x}`)
+})
+$("#show3").on('click',(e) =>{
+	var x = e.target.src.slice(21);
+	$("#show").attr('src',`.${x}`).css({
+		"width":"30%"
+	})	
+})
+function MySlider()
+{
+	var i = 0 ;
+	var index =["1","2","3"]
+	setInterval(() => {
+		$('#show').attr('src',`./img/img${index[i++%3]}.png`)
+	}, 1000);
+}
+
+$('a').on('click',(e) =>
+{
+	e.preventDefault()
+	$('a').append("  Hello !")
+})
+
 
 $('.owl-carousel').owlCarousel({
 	loop:true,
 	margin:10,
-	animateIn:"bounceInLeft",
 	rtl:true,
 	nav:true,
 	autoplay: true,
 	autoplayTimeout: 3000,
+	autoplaySpeed: 2000,
 	autoplayHoverPause: true,
-	center: true,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
-            items:1
+            items:4
         },
         1000:{
-            items:1
+            items:5
         }
     }
 })
